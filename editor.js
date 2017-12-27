ecec6059b091f0291cecf6d887561c427cdc062a
Gil Eckstein <gileck@wix.com>
2017-12-27 15:12:11 +0200

    editor-viewer-application

--- editor.js
+++ editor.js
@@ -1,3 +1,70 @@
-/**
- * Created by gileck on 13/12/2017.
- */
+let editorSDK;
+
+function editorReady(_editorSDK, _appDefinitionId) {
+    editorSDK = _editorSDK;
+    console.log(editorSDK);
+    console.log(_appDefinitionId);
+    const playButtonId = "comp-jb6azaiv";
+    const pauseButtonId = "comp-jb69oeyw";
+    const controllerId = "comp-jb510w6r";
+
+    //TODO: call this function only in the first time....
+    //addController();
+
+    editorSDK.controllers.connect('token', {
+        controllerRef: {
+            type: "DESKTOP", id: controllerId
+        },
+        connectToRef: {
+            type: "DESKTOP", id: playButtonId
+        },
+        role: 'Play'
+    });
+
+    editorSDK.controllers.connect('token', {
+        controllerRef: {
+            type: "DESKTOP", id: controllerId
+        },
+        connectToRef: {
+            type: "DESKTOP", id: pauseButtonId
+        },
+        role: 'Pause'
+    });
+
+
+
+}
+
+function onEvent(event) {
+    if (event === "controllerAdded") {
+        //
+    }
+}
+
+function getAppManifest() {
+    return {
+
+    }
+}
+
+function addController() {
+    editorSDK.pages.getCurrent().then(pageRef => {
+        editorSDK.components.add('token', {
+            componentDefinition: {
+                componentType: 'platform.components.AppController',
+                data: {
+                    applicationId: _appDefinitionId,
+                    controllerType: 'fooBar',
+                    name: 'Item'
+                }
+            },
+            pageRef
+        })
+    });
+}
+
+module.exports = {
+    editorReady,
+    onEvent,
+    getAppManifest
+};
